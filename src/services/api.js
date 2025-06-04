7ea0581d08063f1922210bc3f185cbfefc605491
Shamim Imran <94374652+iamshamimimran@users.noreply.github.com>
2025-06-04 12:33:08 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://localhost:5000/api";
+// const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://game-backend-iipb.onrender.com/api";
 
 export const createGameRoom = async (hostName, maxPlayers) => {
   try {
